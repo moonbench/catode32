2e597c5f484e9431fa999d1c0afd6f86d9195eb6
Moonbench <34589537+moonbench@users.noreply.github.com>
2026-02-12 22:45:12 +1100

    Remove old config values

--- src/config.py
+++ src/config.py
@@ -20,22 +20,12 @@ BTN_MENU1 = 3
 BTN_MENU2 = 2
 
 # Game Constants
-FPS = 30  # Target frames per second
+FPS = 12  # Target frames per second
 FRAME_TIME_MS = 1000 // FPS  # Milliseconds per frame
-
-# Character Constants
-CHAR_SIZE = 8  # Character is 8x8 pixels
-CHAR_SPEED = 2  # Pixels per frame when moving
-
-# World Boundaries
-WORLD_MIN_X = 0
-WORLD_MAX_X = DISPLAY_WIDTH - CHAR_SIZE
-WORLD_MIN_Y = 0
-WORLD_MAX_Y = DISPLAY_HEIGHT - CHAR_SIZE
 
 # Transition Settings
 TRANSITION_TYPE = 'fade'        # 'fade', 'wipe', 'iris'
-TRANSITION_DURATION = 0.5       # seconds per half-transition (total is 2x this)
+TRANSITION_DURATION = 0.4       # seconds per half-transition (total is 2x this)
 
 # Panning Settings
 PAN_SPEED = 2  # pixels per frame when D-pad held
